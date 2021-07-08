073f8d0cc573327c9d799535efebef0a91e03564
1000obo <millobo97@gmail.com>
2021-07-08 10:33:13 +0100

    Pacman Agent WIP
    Main TODOs:
    - restore pellets, ghosts and players positions begin simulation
    - number of observations - too many? dynamic?
    - pacman teleportation verify

--- Assets/Scripts/ML-Agents/PacmanAgent.cs
+++ Assets/Scripts/ML-Agents/PacmanAgent.cs
@@ -7,70 +7,143 @@ using Unity.MLAgents.Actuators;
 
 public class PacmanAgent : Agent
 {
-    //private Pacman API = null;
-    private List<GameObject> pellets;
-    private List<GameObject> largePellets;
-    private List<GameObject> ghosts;
+    private Pacman API = null;
 
-    private Rigidbody2D pacmanBody;
-    private Vector3 initialPosition;
+    [SerializeField]
+    private Rigidbody2D pacmanBody = null;
+    private float speed = 4.0f;
+
+    private Vector2 initialPosition;
 
     private float rewardWinning = 500.0f;
+    private float rewardPellet = 10.0f;
+    private float rewardLargePellet = 100.0f;
+    private float rewardGhost = 200.0f;
+    private float penaltyGhost = -200.0f;
+    private float penaltyDefault = -10.0f;
+
+    private float penaltyWall = -10.0f; //TODO: is it worth it? already penalty for changing state
+    private float rewardFruit = 150.0f; //TODO: cherries?
+
+    public string CollidedActionAgent { private get; set; } = "";
 
     // Start is called before the first frame update
     void Start()
     {
-        //API = gameObject.GetComponent<Pacman>();
-        pacmanBody = GetComponent<Rigidbody2D>();
+        API = GameObject.Find("ML-Agents").GetComponent<Pacman>();
         initialPosition = pacmanBody.transform.localPosition;
     }
 
     public override void OnEpisodeBegin()
     {
-        //reset pacman position
-        //pacmanBody.transform.localPosition = initialPosition;
-        //reset enemies position
-        //reset pellets position
+        //TODO: update positions of pacman and ghosts, restore pellets
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        //Enemies position
-        foreach (GameObject p in ghosts)
-            sensor.AddObservation(p.transform.localPosition);
-        //Pacman position
+        if (API != null) {
+            //ghost
+            AddObservations(sensor, API.GetGhosts());
+            //pellets
+            AddObservations(sensor, API.GetPellets());
+            AddObservations(sensor, API.GetLargePellets());
+        }
+        //player
         sensor.AddObservation(this.transform.localPosition);
-        //Pellets position
+
+        //TODO: update size observations in gameobject?
     }
 
+    void AddObservations(VectorSensor sensor, List<GameObject> lst)
+    {
+        if (lst != null)
+        {
+            foreach (GameObject p in lst)
+                sensor.AddObservation(p.transform.localPosition);
+        }
+    }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        base.OnActionReceived(actions);
+        int directionX = 0;
+        int directionY = 0;
 
-        //Pacman finished game - ate all the dots
-        /*if (pellets.Count == 0 && largePellets.Count == 0)
+        //Get the action index for movement
+        int movement = actions.DiscreteActions[0];
+        // Look up the index in the movement action list:
+        if (movement == 1) { directionY = 1; }
+        else if (movement == 2) { directionY = -1; }
+        else if (movement == 3) { directionX = -1; }
+        else if (movement == 4) { directionX = 1; }
+
+        //Animation
+        GetComponent<Animator>().SetFloat("DirX", directionX);
+        GetComponent<Animator>().SetFloat("DirY", directionY);
+        // Apply the action results to move the Agent
+        pacmanBody.velocity = new Vector2((speed * directionX) * 0.24f, (speed * directionY) * 0.24f);
+
+        //TODO: pacman teleportation verify
+        if (API != null)
         {
-            SetReward(rewardWinning);
-            EndEpisode();
+            List<GameObject> pellets = API.GetPellets();
+            List<GameObject> largePellets = API.GetLargePellets();
+
+            //Pacman finished game - ate all the dots
+            if (pellets.Count == 0 && largePellets.Count == 0)
+            {
+                SetReward(rewardWinning);
+                EndEpisode();
+            }
+            else
+            {
+                if (CollidedActionAgent == "pellet")
+                {
+                    SetReward(rewardPellet); //consumed pellet
+                    CollidedActionAgent = "";
+                }
+                else if (CollidedActionAgent == "largePellet")
+                {
+                    SetReward(rewardLargePellet); //consumed large pellet
+                    CollidedActionAgent = "";
+                }
+                else if (CollidedActionAgent == "ghostDead")
+                {
+                    SetReward(rewardGhost); //captured ghost
+                    CollidedActionAgent = "";
+                }
+                else if (CollidedActionAgent == "ghostAlive")
+                {
+                    SetReward(penaltyGhost); //captured by ghost
+                    CollidedActionAgent = "";
+                    EndEpisode();
+                }
+                else
+                {
+                    SetReward(penaltyDefault); //state of game is changing
+                }
+
+            }
         }
-        else
-        {
-            //Pacman collided with ghost
-            //can eat ghost
-            //cannot eat ghost
-
-            //Pacman collected pellets - small
-            //Pacman collected pellets - big
-
-        }*/
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public override void Heuristic(in ActionBuffers actionsOut)
     {
-        //pellets = API.GetPellets();
-        //largePellets = API.GetLargePellets();
-        //ghosts = API.GetGhosts();
-        
+        var discreteActionsOut = actionsOut.DiscreteActions;
+
+        if (Input.GetKey("up")) {
+            discreteActionsOut[0] = 1; 
+        }
+        else if (Input.GetKey("down"))
+        {
+            discreteActionsOut[0] = 2;
+        }
+        else if (Input.GetKey("left"))
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if (Input.GetKey("right"))
+        {
+            discreteActionsOut[0] = 4;
+        }
     }
 }
